ce61bb42c8d74d22157a50ef5c1056ffb498ec6b
alexbenedic <43232360+alexbenedic@users.noreply.github.com>
2018-10-31 12:04:36 +0800

    add download

---
+++ inc/nonlist_button.php
@@ -0,0 +1,3 @@
+<form>
+<h1 style="padding-right: 50%;">Non Inventories list</h1><div><p> Downlaod here</p>  <button  formaction="../inc/nonlist.php?" ><i class="fa fa-download"></i></button>
+</div></form>
